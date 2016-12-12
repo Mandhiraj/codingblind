256b518d059ab89dd805797fbbe00d1be2a4c9cc
unknown <higginsn@umich.edu>
2016-12-11 23:25:01 -0500

    added code structure checks for lesson 4

--- app/content/content.py
+++ app/content/content.py
@@ -1,5 +1,6 @@
 from lesson1 import lesson1
 from lesson2 import lesson2
 from lesson3 import lesson3
+from lesson4 import lesson4
 
-lessons = [lesson1, lesson2, lesson3]
+lessons = [lesson1, lesson2, lesson3, lesson4]
